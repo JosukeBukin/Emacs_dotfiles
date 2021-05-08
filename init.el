1be08166a34bd8170dc0d17d8e4e2bc160691b53
Илья Иванов <imi200303@gmail.com>
2021-05-08 19:47:17 +0300

    First commit

---
+++ init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit-diff-flycheck magit org-ac company-irony-c-headers company-irony flycheck linum-relative use-package undo-tree neotree evil doom-themes doom-modeline auto-complete)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
